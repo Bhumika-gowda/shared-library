aff49b5809bb3d62b317847ecdf4befe1f9e321d
Bhumika-gowda <bhumikasomashekhar98@gmail.com>
2024-11-10 14:53:24 +0530

    up

--- vars/gitcheckout.groovy
+++ vars/gitcheckout.groovy
@@ -1,13 +1,12 @@
-@Library('mysharedLibrary') _
 
-def gitCheckout(String gitUrl, String credId, String gitBranch) {
+def call(String gitUrl, String credId, String gitBranch) {
     echo "URL: ${gitUrl}"
     checkout([$class: 'GitSCM',
               branches: [[name: gitBranch]],
               doGenerateSubmoduleConfigurations: false,
               extensions: [],
+              submoduleCfg: [],
               userRemoteConfigs: [[url: gitUrl, credentialsId: credId]]
     ])
 }
-
 
